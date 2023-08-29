4c60cc64d4b4ff06f7dee0cff4a72cdca20de6d8
Prakash <py64886@gmail.com>
2023-08-29 13:04:09 +0530

    [Prakash] Added: this is a exception file

---
+++ addressbook/exception/AddressBookException.java
@@ -0,0 +1,15 @@
+package com.addressbook.addressbook.exception;
+
+public class AddressBookException extends RuntimeException {
+
+	public AddressBookException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AddressBookException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
